13dd59f3a2f805f59c5bf03e0096edc78353919a
emmy572 <emmilly.maciel@escola.pr.gov.br>
2024-11-04 16:06:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+window.onload = function () {
+    if (window.location.pathname.endsWith("index.html")) {
+      alert("Bem-vindo ao nosso site sobre Aquecimento Global!");
+    }
+  };
+  
